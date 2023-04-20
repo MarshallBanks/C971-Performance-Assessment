d77e5f9dd8b655d8bd8da6c31df76989b5feafa9
MegaMarsh <marshallmelonhead@hotmail.com>
2023-04-20 16:00:06 -0700

    Add project files.

---
+++ C971 Performance Assessment/ImageResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using System.Reflection;
+
+namespace C971_Performance_Assessment
+{
+    [ContentProperty(nameof(Source))]
+
+    class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            ImageSource imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+
+        }
+    }
+}
